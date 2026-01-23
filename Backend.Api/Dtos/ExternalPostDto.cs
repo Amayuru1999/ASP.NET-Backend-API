b0e45fb03ff496ffa5107f8cdf1b3be916796fe0
Amayuru1999 <amarasingheau@gmail.com>
2026-01-23 23:51:19 +0530

    feat: add ExternalPostDto.cs external API shape #2

--- Backend.Api/Dtos/ExternalPostDto.cs
+++ Backend.Api/Dtos/ExternalPostDto.cs
@@ -2,10 +2,10 @@ namespace Backend.Api.Dtos;
 
 public sealed class ExternalPostDto
 {
-    public int UserId {get; set;}
+    public int UserId {get; init;}
     public int Id {get; init;}
-    public string? Title {get; set;}
-    public string? Body {get; set;}
+    public string? Title {get; init;}
+    public string? Body {get; init;}
 }
 
 
--- Backend.Api/Dtos/PostResponseDto.cs
+++ Backend.Api/Dtos/PostResponseDto.cs
@@ -2,6 +2,7 @@ namespace Backend.Api.Dtos;
 
 public class PostResponseDto
 {
-    public int Id { get; set; }
+    public int Id { get; init; }
     public int UserId { get; init; }
+    public string Title { get; init; }
 }
